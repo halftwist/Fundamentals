de4331e1eca6de9f25692b895ca67f9ca4b8d44e
John Kearon <jkearon@hotmail.com>
2025-03-11 08:00:15 -0400

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by John Kearon on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
