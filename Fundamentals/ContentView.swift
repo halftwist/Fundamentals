14601119793ce5711d02c7b47935b5f4cd0ab6bc
John Kearon <jkearon@hotmail.com>
2025-03-11 09:23:55 -0400

    Swift lesson 29

--- Fundamentals/ContentView.swift
+++ Fundamentals/ContentView.swift
@@ -10,11 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                Circle()
+                    .fill(.blue.gradient)
+                Image(systemName: "swift")
+                    .resizable()
+                    .foregroundStyle(.orange)
+                    .scaledToFit()
+                    .scaleEffect(0.6)
+//                    .background(.blue)
+    //                .border(.orange)
+//                    .padding(.trailing, 30.0)
+            }
+                
+            Text("You Are Awesome!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.red)
+//                .border(.red)
+//                .background(.yellow)
+//            Spacer()
         }
+//        .border(.purple)
         .padding()
     }
 }
